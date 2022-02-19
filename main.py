f1cd2aa9d213012cc815721f0a160f727e6e7453
Zoffyn <ms.firdaus777@gmail.com>
2022-02-20 00:43:37 +0700

    adding player to the game

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-WIDTH, HEIGHT = 50, 30
+from random import randint
+from typing import Final
 
-def render():
-    for i in range(HEIGHT + 2):
-        for j in range(WIDTH + 2):
-            if i == 0 or i == HEIGHT+1 or j == 0 or j == WIDTH+1:
+WIDTH: Final = 50
+HEIGHT: Final = 20
+
+class Position:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+    def __eq__(self, other: 'Position') -> bool:
+        if type(other) is not Position:
+            raise TypeError(type(other))
+        return self.x == other.x and self.y == other.y
+
+    # for debug
+    def __str__(self) -> str:
+        return "Position(" + str(self.x) + ", " + str(self.y) + ")"
+    def __repr__(self) -> str:
+        return "Position(" + str(self.x) + ", " + str(self.y) + ")"
+
+player: list[Position] = [Position(randint(1, WIDTH), randint(1, HEIGHT))]
+print(player[0])
+def render() -> None:
+    for y in range(HEIGHT + 2):
+        for x in range(WIDTH + 2):
+            if x == 0 or x == WIDTH+1 or y == 0 or y == HEIGHT+1:
                 print('█', end='')
             else:
-                print('░', end='')
+                if (pos := Position(x, y)) in player:
+                    if pos == player[0]:
+                        print('▣', end='')
+                    else:
+                        print('□', end='')
+                else:
+                    print('░', end='')
         print()
 
 render()
